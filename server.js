2756b3399ae60ea9d00c5cc2a4c49bcef20bb404
gringrape <gringrape200@gmail.com>
2020-06-20 12:38:05 +0900

    task02_session

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 "use strict";
-
-const express = require("express");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const express     = require("express");
+const fccTesting  = require("./freeCodeCamp/fcctesting.js");
+const session     = require("express-session");
+const passport    = require("passport");
+const dotenv      = require("dotenv");
 
 const app = express();
-
+dotenv.config();
 fccTesting(app); //For FCC testing purposes
+
+app.set('view engine', 'pug');
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// view template engine setup
-app.set('view engine', 'pug');
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
@@ -19,6 +27,7 @@ app.route("/").get((req, res) => {
     title: 'Hello',
     message: 'Please login'
   });
+  console.log(process.env.SESSION_SECRET);
 });
 
 app.listen(process.env.PORT || 3000, () => {
